12cd2b3ee0974521000af45587ccc2c90ee0dd98
enian <eniantafa@gmail.com>
2018-02-21 17:10:48 +0100

    21Feb2018

---
+++ src/AsmpMVC/Migrations/20180221160804_SecondM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+namespace AsmpMVC.Migrations
+{
+    public partial class SecondM : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
